bd19c8fc37e6e283c9e31cb4fa649e7581b1f7ad
SRupa1 <sathiyarupa2005@gmail.com>
2025-07-22 13:30:38 +0530

    Adding Week 6 Handson

---
+++ week 6/myfirstreact/scorecalculatorapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { CalculateScore } from './components/CalculateScore';
+
+function App() {
+  return (
+    <div >
+      <CalculateScore Name={"Rupa"}
+        School={"DNV Public School"}
+        total={284}
+        goal={3} />
+    </div>
+  );
+}
+
+export default App;
